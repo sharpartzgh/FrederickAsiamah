3b31fa27bcf9197531d17565e5a265f1295ae8f9
sharpartzgh <sharpartzprojects@yahoo.com>
2024-03-20 06:38:28 +0000

    hero section update added for mobile and tablets

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import './App.css'
 import Header from './Component/Header/Header'
 import Hero from './Component/Hero/Hero'
+import drop from "../src/assets/drop.png"
 
 
 function App() {
 
   return (
     <body className='mx-auto px-20 bg-black min-h-screen text-white'>
-      <div className="content__wrapper container">
+      <div className="content__wrapper container ">
         <Header/>
+          <div className="image hidden absolute sm:flex sm:left-[230px] sm:top-[50px] md:left-[300px] md:top-[100px] lg:hidden ">
+            <img src={drop} alt="drop"/>
+          </div>
+          <div className="image hidden lg:flex absolute lg:left-[460px] lg:top-[240px] xl:left-[640px] xl:top-[140px]">
+            <img src={drop} alt="drop" />
+          </div>
         <Hero/>
       </div>
     </body>
--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import fred from '../../assets/fred-lg.png'
+import fredmd from '../../assets/fred-md.png'
 import GetInTouch from '../REUSEABLE/GetInTouch'
 
 const Hero = () => {
   return (
-    <div className="text-white mt-5 lg:flex lg:items-center ">
-        <div className="text w-full">
-          <p className=" lg:text-[50px] block">Nice to meet You! I'am</p>
-          <h1 className="lg:text-[55px] xl:text-[70px] xl:font-black block ">FREDERICK ASIAMAH</h1>
-          <p className="lg:text-[25px] lg:w-[445px]"> Based in Ghana, I’m a front-end developer passionate about building things for the web that users love.</p>
+    <div className="text-white mt-5 flex flex-col-reverse lg:flex-row lg:items-center ">
+        <div className="text w-full text-center lg:text-left">
+          <p className=" text-[25px] my-5 md:text-[30px] lg:my-0 lg:text-[50px] block">Nice to meet You! I'am</p>
+          <h1 className="text-[25px] font-black my-5 md:text-[40px] lg:text-[55px] xl:text-[70px] xl:font-black block ">FREDERICK ASIAMAH</h1>
+          <p className=" md:text-[18px] md:px-20 lg:px-0 lg:text-[25px] lg:w-[445px]"> Based in Ghana, I’m a front-end developer passionate about building things for the web that users love.</p>
           <GetInTouch text="GET IN TOUCH"/>
         </div>
         {/* for mobile and tablets */}
+        <div className="image w-full flex items-center mt-16 md:mt-28 justify-center lg:hidden">
+          <img src={fredmd} alt="fred's-picture" className=" w-[378.59px]" />
+        </div>
         {/* for large screens */}
         <div className="image hidden w-full lg:contents">
           <img src={fred} alt="fred's-picture" className="lg:w-[349px] lg:h-[345px] xl:w-[449px] xl:h-[545px]" />
