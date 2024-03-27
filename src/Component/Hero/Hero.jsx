6757d29afc5aed61fbaebde0768471598f2f8853
sharpartzgh <sharpartzprojects@yahoo.com>
2024-03-27 07:06:10 +0000

    update

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className="text-white mt-5 flex flex-col-reverse lg:flex-row lg:items-center ">
         <div className="text w-full text-center lg:text-left z-50">
-          <p className=" text-[25px] my-5 md:text-[30px] lg:my-0 lg:text-[50px] block">Nice to meet You! I'am</p>
+          <p className=" text-[25px] my-5 md:text-[30px] lg:my-0 lg:text-[50px] block">Nice to meet You! I'm</p>
           <h1 className="text-[25px] font-black my-5 md:text-[40px] lg:text-[55px] xl:text-[70px] xl:font-black block ">FREDERICK ASIAMAH</h1>
           <p className=" md:text-[18px] md:px-20 lg:px-0 lg:text-[25px] lg:w-[445px]"> Based in Ghana, I’m a front-end developer passionate about building things for the web that users love.</p>
           <GetInTouch text="GET IN TOUCH"/>
